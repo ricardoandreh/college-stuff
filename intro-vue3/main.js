94b005a74881b3481cb87fd22f6582a777598414
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-06-28 10:50:42 -0300

    intro-vue3: tutorial 5

--- intro-vue3/main.js
+++ intro-vue3/main.js
@@ -7,12 +7,18 @@ const app = createApp({
     const inventory = ref(100);
     const product = ref("Socks");
     const image = ref("./assets/images/socks_green.jpg");
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const url = ref("https://www.linkedin.com/in/ricardoandreh/");
-    
+    const sizes = ref(["S", "M", "L", "XL"]);
+    const variants = ref([
+      { id: 2234, color: "green" }, 
+      { id: 2235, color: "blue" }, 
+    ]);
+
     return {
-      product, image, 
-      url, inStock, 
-      inventory, onSale, 
+      product, image, url, 
+      inStock, inventory, onSale, 
+      details, variants, sizes, 
     }
   }
 });
