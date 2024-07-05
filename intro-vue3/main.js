162ca21b8c0bce53275c1791c1a3768356126d5b
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-07-05 18:47:52 -0300

    intro-vue3: tutorial 6

--- intro-vue3/main.js
+++ intro-vue3/main.js
@@ -2,6 +2,7 @@ const { createApp, ref } = Vue;
 
 const app = createApp({
   setup() {
+    const cart = ref(0);
     const onSale = ref(true);
     const inStock = ref(true);
     const inventory = ref(100);
@@ -11,14 +12,20 @@ const app = createApp({
     const url = ref("https://www.linkedin.com/in/ricardoandreh/");
     const sizes = ref(["S", "M", "L", "XL"]);
     const variants = ref([
-      { id: 2234, color: "green" }, 
-      { id: 2235, color: "blue" }, 
+      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" }, 
+      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" }, 
     ]);
+
+    const addToCart = () => cart.value++;
+    const removeFromCart = () => cart.value > 0 && cart.value--;
+    const updateImage = (variantImage) => image.value = variantImage;
 
     return {
       product, image, url, 
       inStock, inventory, onSale, 
       details, variants, sizes, 
+      cart, addToCart, updateImage, 
+      removeFromCart, 
     }
   }
 });
