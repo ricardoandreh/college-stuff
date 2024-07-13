379601f009aa29864d8e449674039a9f85b927eb
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-07-12 23:03:35 -0300

    intro-vue3: tutorial 9

---
+++ intro-vue3/components/ProductDisplay.js
@@ -0,0 +1,111 @@
+app.component("product-display", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const onSale = ref(true);
+    const inventory = ref(11);
+    const product = ref("Socks");
+    const selectedVariant = ref(0);
+    const brand = ref("Vue Mastery");
+    const url = ref("https://www.linkedin.com/in/ricardoandreh/");
+    const variants = ref([
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/images/socks_green.jpg",
+        quantity: 50,
+      },
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/images/socks_blue.jpg",
+        quantity: 0,
+      },
+    ]);
+
+    const title = computed(() => brand.value + " " + product.value);
+    const image = computed(() => variants.value[selectedVariant.value].image);
+    const inStock = computed(
+      () => variants.value[selectedVariant.value].quantity
+    );
+    const shipping = computed(() => (props.premium ? "Free" : 2.99));
+
+    const addToCart = () => cart.value++;
+    const removeFromCart = () => cart.value > 0 && cart.value--;
+    const updateVariant = (index) => (selectedVariant.value = index);
+
+    return {
+      onSale, product, selectedVariant, 
+      brand, url, variants, title, image, 
+      inStock, shipping, inventory, addToCart, 
+      removeFromCart, updateVariant, 
+    };
+  },
+  template: `
+    <div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <img
+            :class="[ !inStock ? 'out-of-stock-img' : '' ]"
+            :src="image"
+          />
+        </div>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+
+          <p v-if="inventory > 10">Em Estoque</p>
+          <p v-else-if="inventory <= 10 && inventory > 0">
+            Quase esgotado!
+          </p>
+          <p v-else>Fora de Estoque</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <p v-show="onSale">{{ title }} is on sale.</p>
+
+          <ul v-for="detail, index in details" :key="index">
+            <li>{{ detail }}</li>
+          </ul>
+
+          <!-- <ul v-for="size in sizes">
+            <li>{{ size }}</li>
+          </ul> -->
+
+          <div
+            v-for="variant, index in variants"
+            :key="variant.id"
+            @mouseover="updateVariant(index)"
+            class="color-circle"
+            :style="{ backgroundColor: variant.color }"
+          ></div>
+
+          <button
+            class="button"
+            :class="{ disabledButton: !inStock }"
+            :disabled="!inStock"
+            @click="addToCart"
+          >
+            Add to Cart
+          </button>
+          <button
+            class="button"
+            :class="{ disabledButton: cart <= 0 }"
+            :disabled="cart <= 0"
+            @click="removeFromCart"
+          >
+            Remove Item
+          </button>
+          <br />
+          <a :href="url" target="_blank">Made by Ricardo André</a>
+        </div>
+      </div>
+    </div>`,
+});
--- intro-vue3/main.js
+++ intro-vue3/main.js
@@ -3,35 +3,11 @@ const { createApp, ref, computed } = Vue;
 const app = createApp({
   setup() {
     const cart = ref(0);
-    const onSale = ref(true);
-    const inventory = ref(100);
-    const product = ref("Socks");
-    const selectedVariant = ref(0);
-    const brand = ref("Vue Mastery");
+    const premium = ref(true);
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-    const url = ref("https://www.linkedin.com/in/ricardoandreh/");
-    const sizes = ref(["S", "M", "L", "XL"]);
-    const variants = ref([
-      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 }, 
-      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 }, 
-    ]);
 
-    const title = computed(() => brand.value + ' ' + product.value);
-    const image = computed(() => variants.value[selectedVariant.value].image);
-    const inStock = computed(() => variants.value[selectedVariant.value].quantity);
-    
-    const addToCart = () => cart.value++;
-    const removeFromCart = () => cart.value > 0 && cart.value--;
-    const updateImage = (variantImage) => image.value = variantImage;
-    const updateVariant = (index) => selectedVariant.value = index;
-    
     return {
-      product, image, url, 
-      inStock, inventory, onSale, 
-      details, variants, sizes, 
-      cart, addToCart, updateImage, 
-      removeFromCart, brand, title, 
-      selectedVariant, updateVariant, 
-    }
-  }
+      cart, premium, details
+    };
+  },
 });
