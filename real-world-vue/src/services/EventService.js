e6a3151555596f5e57acf12dbad6d62718d1e80b
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-07-26 09:42:35 -0300

    Revert "fix(real-world-vue): test new endpoint again - tutorial 5"
    This reverts commit 1d80ad6843bcf37bf21812df26ded822bb1364b1.

--- real-world-vue/src/services/EventService.js
+++ real-world-vue/src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/ricardoandreh/college-stuff/blob/main/real-world-vue/db.json',
+  baseURL: 'https://my-json-server.typicode.com/ricardoandreh/college-stuff',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
