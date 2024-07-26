ea7a9d3f80451ecc9f73a966a0414c8d9dd8cc86
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-07-26 09:27:59 -0300

    fix(real-world-vue): tutorial 5

--- real-world-vue/src/services/EventService.js
+++ real-world-vue/src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/ricardoandreh/college-stuff/tree/main/real-world-vue',
+  baseURL: 'https://my-json-server.typicode.com/ricardoandreh/college-stuff',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
