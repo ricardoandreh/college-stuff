7eb41d8f27ae03ae5c8ad0bf8dea433259c1f6f8
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-08-15 20:26:50 -0300

    refactor: clean up the repo

--- grande_software/core/guitar.py
+++
@@ -1,7 +0,0 @@
-from core.guitar_spec import GuitarSpec
-from core.instrument import Instrument
-
-
-class Guitar(Instrument):
-    def __init__(self, serial_number: str, price: float, spec: GuitarSpec):
-        super().__init__(serial_number, price, spec)
